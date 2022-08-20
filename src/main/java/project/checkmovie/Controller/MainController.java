dfe553e1a029a21293ae49e3c5e5a129fd2b4caa
JURI <jr4426@naver.com>
2022-08-20 12:46:48 +0900

    오류

---
+++ src/main/java/project/checkmovie/Controller/MainController.java
@@ -0,0 +1,16 @@
+package project.checkmovie.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/main")
+    public String index(){
+        return "01-main-origin";
+    }
+}
